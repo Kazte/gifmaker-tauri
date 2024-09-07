90d9d715fe3b7e67a783b91cd3c4adc000559806
Kazte <39470734+Kazte@users.noreply.github.com.>
2024-09-07 18:44:27 -0300

    feat: updater

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -9,6 +9,7 @@ fn load_video(file_path: String) -> Result<Vec<u8>, String> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![load_video])
